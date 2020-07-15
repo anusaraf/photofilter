af7550def4cc4e134a2135ee5caa55675a397672
anusaraf <66026828+anusaraf@users.noreply.github.com>
2020-07-15 09:20:09 +0530

    Add files via upload

---
+++ abouts.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width,initial-scale=1" />
+    <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css" />
+    <link rel="stylesheet" href="style.css" />
+    <link  href="about.php" />
+    <title>
+        Online Image Effect
+    </title>
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="abouts.php">About</a>
+                </li>
+               
+            </ul>
+           
+        </div>
+    </nav>
+
+
+     <div class="container-fluid ">
+        <div class="jumbotron" style="background:blue;">
+
+            <h1>Amazing Online Photo Filter Editor</h1>
+           
+               <h3> <p><b> Make your image more stunning and attractive by following features </b></p></h3>
+           
+           <div>
+           <div class="row">
+            <div class=" col-md-6 m-auto text-left">
+           <h4>        1) Beautiful editing users start here!<br>
+                       2) Modern photo editing for quick filters, and beginners.<br>
+                       3) Open most image like PSD (Photoshop), PXD, JPEG, PNG.<br>
+                       4) Most tools in a few sections.<br>
+                       5) Perfect for quick, playful photo filters.<br>
+                       6) One-click to add artistic effects to photos.<br>
+                       7) Croping image and then apply filters and effects along with reset button.<br>
+                      
+                       
+</h4>
+</div>
+<div class=" col-md-5 m-auto text-right">
+ <img src="imp.jpg" class=" img-fluid" alt="Effect Image" style="text-align:right " />
+
+ </div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+</div>
+            </div>
+            <br />
+           
+        </div>
+
+    </div>
+
+
+    </body>
+    </html>
